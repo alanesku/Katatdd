f0bdd98e01b4693f94240f75dca42429c070dd0c
Alan <alaneskumobile@gmail.com>
2017-04-05 11:37:18 +0200

    Si le paso un uno devuelve uno

---
+++ katafizzbuzz/src/katafizzbuzz/fizzbuzz.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package katafizzbuzz;
+
+/**
+ *
+ * @author daw117
+ */
+public class fizzbuzz {
+    
+    public int getfizzbuzz(int n)
+    {
+        return 1;
+    }
+    
+}
